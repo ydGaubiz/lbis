eb6daca615cb9f0bae3e7a68d9d54b6814a38b58
ydGaubiz <dhyoon@gaubiz.com>
2016-02-18 12:26:47 +0900

    2/18 mainActivity 주석 추가

---
+++ app/src/main/java/gaubiz/lbis/WebViewMapActivity.java
@@ -0,0 +1,30 @@
+package gaubiz.lbis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class WebViewMapActivity extends Activity {
+
+    private WebView mWebView;
+
+    private String url = "http://m.naver.com/";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webviewmap);
+
+        mWebView = (WebView) findViewById(R.id.webView);
+
+        //웹뷰 호출
+        mWebView.getSettings().setJavaScriptEnabled(true); // 웹뷰에서 자바 스크립트 사용
+        mWebView.loadUrl(url);
+        mWebView.setWebViewClient(new WebViewClient());
+
+/*        MainActivity ma = (MainActivity) getApplicationContext();
+        ma.onResume();*/
+    }
+
+}//end
